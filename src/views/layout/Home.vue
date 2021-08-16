c76a492936d4098c3d7b8d73138191c7d7172ab8
13577419751 <1760091305@qq.com>
2021-08-16 21:36:57 +0800

    系统初始化布局

---
+++ src/views/layout/Home.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="home">
+
+    <!--左侧内容-->
+    <left-menu />
+    <!--右侧内容-->
+    <div :class="{'main-app':true,'menu-unfold':$store.state.collapsed}">
+      <slider-nav />
+      <router-view></router-view>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import LeftMenu from './components/menu.vue';
+import SliderNav from './components/sliderNav.vue';
+
+export default {
+  data() {
+    return {
+
+    };
+  },
+  components: {
+    LeftMenu,
+    SliderNav,
+  },
+  methods: {
+
+  },
+};
+</script>
+
+<style lang="less">
+@import url("~@/assets/css/home.less");
+
+</style>
